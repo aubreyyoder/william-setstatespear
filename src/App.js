7c30642eaac55658d3809449cc533a7587e18a83
Aubrey Yoder <Aubreyyoder@gmail.com>
2019-05-28 14:53:39 -0700

    Adding Router to application

--- src/App.js
+++ src/App.js
@@ -1,19 +1,22 @@
-import React, { Component } from 'react';
-import PoemListPage from './PoemListPage'
-import './App.css';
+import React, { Component } from "react";
+import PoemListPage from "./PoemListPage";
+import PoemPage from "./PoemPage";
+import { Route } from "react-router-dom";
+import "./App.css";
 
 class App extends Component {
   render() {
     return (
-      <div className='App'>
+      <div className="App">
         <nav>
-          <a href='/'>Poem List</a>
+          <a href="/">Poem List</a>
         </nav>
         <header>
           <h1>William Setstatespear</h1>
         </header>
         <main>
-          <PoemListPage />
+          <Route exact path="/" component={PoemListPage} />
+          <Route path="/poem/:poemId" component={PoemPage} />
         </main>
         <footer>
           <p>© WilliamSetstatespear, 2018. All Rights Reserved.</p>
